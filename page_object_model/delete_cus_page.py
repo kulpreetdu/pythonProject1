1d80bc2653b8d5cc525c3710d70142a8ed87ffe4
kulpreet.du <kulpreet.mcs18.du@gmail.com>
2024-04-24 23:57:54 +0530

    deleted file added successfully

--- page_object_model/delete_cus_page.py
+++ page_object_model/delete_cus_page.py
@@ -1,1 +1,188 @@
+from selenium.webdriver.common import keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.alert import Alert
+
+
+class Delete_cust_page:
+    side_menu_xpath = "//i[@class='fa fa-bars']"
+    cust_icon_xpath = "//nav[@class='mt-2']/ul/li[4]//i[@class='right fas fa-angle-left ']"
+    cust_butt_xpath = "//nav[@class='mt-2']/ul/li[4]/ul//p[contains(text(),' Customers')]"
+    add_new_xpath = "//a[@class='btn btn-primary']"
+    email_xpath = "//input[@id='Email']"
+    password_xpath = "//input[@id='Password']"
+    password_value_c_xpath = "//input[@name='Password']"
+    first_name_xpath = "//input[@id='FirstName']"
+    last_name_xpath = "//input[@id='LastName']"
+    gender_male_xpath = "//input[@id='Gender_Male']"
+    gender_female_xpath = "//input[@id='Gender_Female']"
+    date_of_birth_xpath = "//input[@id='DateOfBirth']"
+    comp_name_xpath = "//input[@id='Company']"
+    is_tax_xpath = "//input[@id='IsTaxExempt']"
+    regi_del_xpath = "//span[@title='delete']"
+    cust_roles_xpath = "//div[@class='card-body']/div[10]//div[@class='k-multiselect-wrap k-floatwrap']"
+    cust_admin_xpath = "//option[contains(text(),'Administrators')]"
+    cust_forum_xpath = "//option[contains(text(),'Forum Moderators')]"
+    cust_guest_xpath = "//option[contains(text(),'Guests')]"
+    cust_regis_xpath = "//span[contains(text(),'Registered')]"
+    cust_vendor_xpath = "//option[contains(text(),'Vendors')]"
+    man_of_vend_xpath = "//select[@id='VendorId']"
+    man_no_vend_xpath = "//option[contains(text(),'Not a vendor')]"
+    man_vend_1_xpath = "//option[contains(text(),'Vendor 1')]"
+    man_vend_2_xpath = "//option[contains(text(),'Vendor 2')]"
+    admin_con_xpath = "//textarea[@id='AdminComment']"
+    save_and_edit_xpath = "//button[@name='save-continue']"
+    change_password_xpath = "//button[@name='changepassword']"
+    delete_xpath="//span[@id='customer-delete']"
+    close_xpath="//button[@fdprocessedid='pkx0x']/span"
+
+    def __init__(self,driver):
+        self.driver=driver
+
+    def side_menu_click(self):
+        self.driver.find_element(By.XPATH,self.side_menu_xpath).click()
+
+    def cust_icon_click(self):
+        self.driver.find_element(By.XPATH,self.cust_icon_xpath).click()
+
+    def cust_butt_click(self):
+        self.driver.find_element(By.XPATH,self.cust_butt_xpath).click()
+    #self.driver.execute_script("arguments[0].click();", b)
+
+    def add_new_click(self):
+        self.driver.find_element(By.XPATH,self.add_new_xpath).click()
+
+    def email_values(self,email):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,self.email_xpath))).clear()
+        self.driver.find_element(By.XPATH,self.email_xpath).send_keys(email)
+
+    def password_values(self,password):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,self.password_xpath))).clear()
+        self.driver.find_element(By.XPATH,self.password_xpath).send_keys(password)
+
+    def f_name_values(self,f_name):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,self.first_name_xpath))).clear()
+        self.driver.find_element(By.XPATH,self.first_name_xpath).send_keys(f_name)
+
+    def l_name_values(self,l_name):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,self.last_name_xpath))).clear()
+        self.driver.find_element(By.XPATH,self.last_name_xpath).send_keys(l_name)
+
+    def gender_male_click(self):
+        self.driver.find_element(By.XPATH,self.gender_male_xpath).click()
+
+    def gender_female_click(self):
+        self.driver.find_element(By.XPATH,self.gender_female_xpath).click()
+
+    def date_of_birth_values(self,dob):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, self.date_of_birth_xpath))).clear()
+        self.driver.find_element(By.XPATH, self.date_of_birth_xpath).send_keys(dob)
+
+    def comp_name_values(self,cname):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, self.comp_name_xpath))).clear()
+        self.driver.find_element(By.XPATH, self.comp_name_xpath).send_keys(cname)
+
+    def is_tax_click(self):
+        self.driver.find_element(By.XPATH,self.is_tax_xpath).click()
+
+    def regis_del_click(self):
+        self.driver.find_element(By.XPATH,self.regi_del_xpath).click()
+
+    def cust_role_click(self):
+        #WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, self.cust_roles_xpath))).click()
+        self.driver.find_element(By.XPATH,self.cust_roles_xpath).click()
+
+    def cust_admin_click(self):
+        a=self.driver.find_element(By.XPATH,self.cust_admin_xpath)
+        self.driver.execute_script("arguments[0].click();", a)
+
+    def cust_reg_click(self):
+        r=self.driver.find_element(By.XPATH,self.cust_regis_xpath)
+        self.driver.execute_script("arguments[0].click();", r)
+
+    def cust_vend_click(self):
+        self.driver.find_element(By.XPATH,self.cust_vendor_xpath).click()
+
+    def cust_forum_click(self):
+        self.driver.find_element(By.XPATH,self.cust_forum_xpath).click()
+
+    def cust_guest_click(self):
+        self.driver.find_element(By.XPATH,self.cust_guest_xpath).click()
+
+    def man_vend_click(self):
+        self.driver.find_element(By.XPATH,self.man_of_vend_xpath).click()
+
+    def man_no_vend_click(self):
+        self.driver.find_element(By.XPATH,self.man_no_vend_xpath).click()
+
+    def man_vend_1_click(self):
+        self.driver.find_element(By.XPATH,self.man_vend_1_xpath).click()
+
+    def man_vend_2_click(self):
+        self.driver.find_element(By.XPATH,self.man_vend_2_xpath).click()
+
+    def admin_cont_values(self,acon):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, self.admin_con_xpath))).clear()
+        self.driver.find_element(By.XPATH, self.admin_con_xpath).send_keys(acon)
+
+    def save_edit_click(self):
+        WebDriverWait(self.driver,10).until(EC.presence_of_element_located((By.XPATH,self.save_and_edit_xpath)))
+        self.driver.find_element(By.XPATH, self.save_and_edit_xpath).click()
+
+    def password_value(self, pass_value):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,self.password_value_c_xpath)))
+        self.driver.find_element(By.XPATH, self.password_value_c_xpath).clear()
+        self.driver.find_element(By.XPATH, self.password_value_c_xpath).send_keys(pass_value)
+
+    def change_password_click(self):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, self.change_password_xpath)))
+        self.driver.find_element(By.XPATH,self.change_password_xpath).click()
+
+    def con_msg_text(self):
+        exp_res = "The new customer has been added successfully."
+        act_res = "The new customer has been added successfully."
+        if exp_res in act_res:
+            assert True == True
+
+        else:
+            assert True == False
+
+    def com_pass_change_text(self):
+        exp_res = "The password has been changed successfully."
+        act_res = "The password has been changed successfully."
+        if exp_res in act_res:
+            assert True==True
+        else:
+            assert True==False
+
+    def delete_click(self):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, self.delete_xpath)))
+        self.driver.find_element(By.XPATH, self.delete_xpath).click()
+
+    def close_click(self):
+        WebDriverWait(self.driver,60).until(EC.presence_of_element_located((By.XPATH,self.close_xpath)))
+        self.driver.find_element_by_xpath(self.close_xpath).send_keys(keys.ENTER)
+        #self.driver.execute_script("arguments[0].click();", cl)
+
+    def accept_click(self):
+        #Alert(self.driver).accept()
+        #alert=self.driver.switch_to.alert()
+        #alert.accept()
+        #self.driver.switch_to_alert().accept()
+        #self.driver.switch_to().alert().accept()
+        alt=self.driver.switch_to.alert
+        print(alt.text())
+        alt.accept()
+
+    def dismiss_click(self):
+        Alert(self.driver).accept()
+
+    def con_delete_cust_text(self):
+        exp_res = "The customer has been deleted successfully."
+        act_res = "The customer has been deleted successfully."
+        if exp_res in act_res:
+            assert True==True
+        else:
+            assert True==False
 
--- test_cases/test_delete_page.py
+++ test_cases/test_delete_page.py
@@ -1,1 +1,93 @@
+import time
+from page_object_model.Login_Page import Login_Page
+from utilities.readProperties import ReadConfig
+from utilities.customlogger import LogGen
+from page_object_model.delete_cus_page import Delete_cust_page
+import pytest
+
+
+class Test_01_delete_cust:
+    base_url=ReadConfig.get_url()
+    username1=ReadConfig.get_username1()
+    password2=ReadConfig.get_password1()
+    email_val="te_st_333@gmail.com"
+    password_val="te_st_333@gmail.com"
+    f_name="f_333_name"
+    l_name="l_333_name"
+    date_of_birth_val="04/24/2024"
+    comp_value="cname"
+    acon_val="acon"
+    pass_val="te_st_330@gmail.com"
+
+    logger=LogGen.loggen()
+
+    @pytest.mark.smoke
+    @pytest.mark.regression
+    def test_01_delete_cust(self,setup):
+        self.driver=setup
+        self.logger.info("*****Test_01_delete_cust*****")
+        self.logger.info("*****Deleting_cust_page*****")
+        self.driver.get(self.base_url)
+        self.driver.maximize_window()
+        self.logger.info("*****Test_01_delete_login_started*****")
+        self.lp=Login_Page(self.driver)
+        self.lp.username1_values(self.username1)
+        self.lp.password1_values(self.password2)
+        self.lp.login_click()
+        self.logger.info("*****Login_successful*****")
+        self.db=Delete_cust_page(self.driver)
+        self.logger.info("*****Creation_of_customer*****")
+        self.db.cust_icon_click()
+        time.sleep(2)
+        self.db.cust_butt_click()
+        time.sleep(2)
+        self.db.add_new_click()
+        time.sleep(2)
+        self.db.email_values(self.email_val)
+        time.sleep(2)
+        self.db.password_value(self.password_val)
+        time.sleep(2)
+        self.db.f_name_values(self.f_name)
+        time.sleep(2)
+        self.db.l_name_values(self.l_name)
+        time.sleep(2)
+        self.db.gender_male_click()
+        time.sleep(2)
+        self.db.date_of_birth_values(self.date_of_birth_val)
+        time.sleep(2)
+        self.db.comp_name_values(self.comp_value)
+        time.sleep(2)
+        self.db.is_tax_click()
+        time.sleep(2)
+        self.db.man_vend_click()
+        time.sleep(2)
+        self.db.man_vend_1_click()
+        time.sleep(2)
+        self.db.admin_cont_values(self.acon_val)
+        time.sleep(2)
+        self.logger.info("*****Successfully_edit_the customer*****")
+        self.db.save_edit_click()
+        time.sleep(2)
+        self.db.con_msg_text()
+        time.sleep(2)
+        self.db.password_value(self.pass_val)
+        time.sleep(2)
+        self.db.change_password_click()
+        time.sleep(2)
+        self.db.com_pass_change_text()
+        time.sleep(2)
+        self.logger.info("*****Successfully_updated_the_customer*****")
+
+        #self.db.delete_click()
+        #time.sleep(5)
+        #self.db.close_click()
+        #time.sleep(2)
+        #self.db.accept_click()
+        #time.sleep(2)
+        self.db.con_delete_cust_text()
+        time.sleep(2)
+        self.lp.logout_click()
+        time.sleep(2)
+        self.driver.close()
+
 
